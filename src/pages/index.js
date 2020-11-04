46afaf69685fc591603eb84c6b7e9dff75f22fc8
saihaj <saihajpreet.singh@gmail.com>
2020-11-04 12:54:27 -0600

    feat: add products to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,49 @@ import useBaseUrl from '@docusaurus/useBaseUrl'
 
 import styles from './styles.module.css'
 
+const prodcuts = [
   {
+    title: 'Database',
+    description: 'The main product',
   },
   {
+    title: 'Presenter',
+    description: 'Desktop App',
   },
   {
+    title: 'Mobile',
+    description: 'Mobile App',
+  },
+  {
+    title: 'Viewer',
+    description: 'Web app to see DB and report errors',
+  },
+  {
+    title: 'Gurmukhi Utils',
+    description: 'Good lib',
+  },
+  {
+    title: 'Theme Tool',
+    description: 'One of its kind to make themes',
+  },
+  {
+    title: 'Tutorials',
+    description: 'For not so advanced folks',
+  },
+  {
+    title: 'Contribute',
+    description: 'Make world a better place!',
   },
 ]
 
+const Product = ( { title, description } ) => (
+  <div className={clsx( 'col col--4', styles.feature )}>
+    <div className="text--center">
+      <h2>{title}</h2>
       <p>{description}</p>
     </div>
+  </div>
+)
 
 const Home = () => {
   const context = useDocusaurusContext()
@@ -48,7 +81,15 @@ const Home = () => {
       </header>
 
       <main>
+        <section className={styles.features}>
+          <div className="container">
+            <div className="row">
+              { prodcuts.map( ( { title, description } ) => (
+                <Product key={title} title={title} description={description} />
+              ) )}
             </div>
+          </div>
+        </section>
       </main>
     </Layout>
   )
